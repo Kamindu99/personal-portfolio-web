f81a1038ab487702e9cd533932e18e4b043b0fc6
kamindu.g <kamindu.g@informaticsint.com>
2023-06-21 02:23:33 +0530

    my projects

---
+++ src/pages/MyProfile.tsx
@@ -0,0 +1,113 @@
+import React from 'react'
+
+type Props = {}
+
+const MyProfile = (props: Props) => {
+  return (
+    <div>
+           <body>
+<div className="container-lg mt-5">
+	<div className="row">
+		<div className="col-lg-3">
+			<div className="accordion mb-2" id="accordionExample">
+				<div className="card">
+					<div className="card-header" id="headingOne">
+						<h2 className="mb-0">									
+							<a data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+								<span><i className="fa fa-user"></i> Account</span>
+								<i className="fa fa-chevron-down toggle"></i>
+							</a>
+						</h2>
+					</div>
+					<div id="collapseOne" className="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+						<div className="card-body">
+							<ul className="list-group">
+								<li className="list-group-item"><a href="#"><i className="fa fa-pencil"></i> Edit Info</a></li>
+								<li className="list-group-item"><a href="#"><i className="fa fa-key"></i> Change Password</a></li>
+								<li className="list-group-item"><a href="#" className="text-danger"><i className="fa fa-trash"></i> Delete Account</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div className="card">
+					<div className="card-header" id="headingTwo">
+						<h2 className="mb-0">									
+							<a data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+								<span><i className="fa fa-comments"></i> Messages</span>
+								<i className="fa fa-chevron-down toggle rotate"></i>
+							</a>
+						</h2>
+					</div>
+					<div id="collapseTwo" className="collapse show" aria-labelledby="headingTwo" data-parent="#accordionExample">
+						<div className="card-body">
+							<ul className="list-group">
+								<li className="list-group-item"><a href="#"><i className="fa fa-inbox"></i> Inbox <span className="badge badge-pill badge-primary">20</span></a></li>
+								<li className="list-group-item"><a href="#"><i className="fa fa-paper-plane"></i> Sent</a></li>
+								<li className="list-group-item"><a href="#"><i className="fa fa-file-text"></i> Drafts <span className="badge badge-pill badge-info">15</span></a></li>
+								<li className="list-group-item"><a href="#"><i className="fa fa-trash"></i> Trash</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div className="card">
+					<div className="card-header" id="headingThree">
+						<h2 className="mb-0">									
+							<a data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+								<span><i className="fa fa-bar-chart"></i> Reports</span>
+								<i className="fa fa-chevron-down toggle"></i>
+							</a>
+						</h2>
+					</div>
+					<div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+						<div className="card-body">
+							<ul className="list-group">										
+								<li className="list-group-item"><a href="#"><i className="fa fa-dollar"></i> Sales</a></li>
+								<li className="list-group-item"><a href="#"><i className="fa fa-tags"></i> Orders</a></li>
+								<li className="list-group-item"><a href="#"><i className="fa fa-plane"></i> Shipment</a></li>
+								<li className="list-group-item"><a href="#"><i className="fa fa-users"></i> Customers</a></li>										
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div className="card">
+					<div className="card-header" id="headingFour">
+						<h2 className="mb-0">									
+							<a data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+								<span><i className="fa fa-cog"></i> Settings</span>
+								<i className="fa fa-chevron-down toggle"></i>
+							</a>
+						</h2>
+					</div>
+					<div id="collapseFour" className="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+						<div className="card-body">
+							<ul className="list-group">
+								<li className="list-group-item"><a href="#"><i className="fa fa-font"></i> Typography</a></li>
+								<li className="list-group-item"><a href="#"><i className="fa fa-bell"></i> Notifications</a></li>
+								<li className="list-group-item"><a href="#"><i className="fa fa-map"></i> Maps</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div className="card">
+					<div className="card-header" id="headingFive">
+						<h2 className="mb-0"><a href="#"><i className="fa fa-power-off"></i> Logout</a></h2>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div className="col-lg-9">
+			<div className="jumbotron">
+				<h1 className="display-4">Hello, world!</h1>
+				<p className="lead">This is a simple example of accordion menu that can easily be integrated into any Bootstrap template. You'll find many such examples at <a href="https://www.tutorialrepublic.com" target="_blank">tutorialrepublic.com</a>.</p>
+				<hr className="my-4"/>						
+				<a className="btn btn-primary btn-lg" href="https://www.tutorialrepublic.com/snippets/gallery.php" target="_blank">Learn more</a>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+    </div>
+  )
+}
+
+export default MyProfile
--- src/pages/MyProjects.tsx
+++ src/pages/MyProjects.tsx
@@ -3,9 +3,70 @@ import React from 'react'
 type Props = {}
 
 const MyProjects = (props: Props) => {
+
+    const [projects, setProjects] = React.useState([
+        {
+            id: "headingOne",
+            target: "collapseOne",
+            title: "Computer Shop Management System",
+            description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+            image: "https://res.cloudinary.com/dmfljlyu1/image/upload/v1685267799/travelwebkamindu_r6xqqh.jpg",
+            url: "https://miro.medium.com/v2/resize:fit:2400/1*Atv7s1TO3VSEfNpq_2nc1A.jpeg"
+        },
+        {
+            id: "headingTwo",
+            target: "collapseTwo",
+            title: "Tourism Management System",
+            description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+            image: "https://res.cloudinary.com/dmfljlyu1/image/upload/v1685799530/dotcomecomputers_rzpgyg.jpg",
+            url: "https://miro.medium.com/v2/resize:fit:2400/1*Atv7s1TO3VSEfNpq_2nc1A.jpeg"
+        },
+        {
+            id: "headingThree",
+            target: "collapseThree",
+            title: "Personal Portfolio Website",
+            description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+            image: "https://res.cloudinary.com/dmfljlyu1/image/upload/v1687292391/potfolioappmy_imyf7n.jpg",
+            url: "https://miro.medium.com/v2/resize:fit:2400/1*Atv7s1TO3VSEfNpq_2nc1A.jpeg"
+        },
+        {
+            id: "headingFour",
+            target: "collapseFour",
+            title: "Agriculture Management System",
+            description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+            image: "https://res.cloudinary.com/dmfljlyu1/image/upload/v1687292391/potfolioappmy_imyf7n.jpg",
+            url: "https://miro.medium.com/v2/resize:fit:2400/1*Atv7s1TO3VSEfNpq_2nc1A.jpeg"
+        },
+        {
+            id: "headingFive",
+            target: "collapseFive",
+            title: "Travel Management System",
+            description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+            image: "https://res.cloudinary.com/dmfljlyu1/image/upload/v1687292391/potfolioappmy_imyf7n.jpg",
+            url: "https://miro.medium.com/v2/resize:fit:2400/1*Atv7s1TO3VSEfNpq_2nc1A.jpeg"
+        },
+        {
+            id: "headingSix",
+            target: "collapseSix",
+            title: "Hospital Management System",
+            description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+            image: "https://res.cloudinary.com/dmfljlyu1/image/upload/v1687292391/potfolioappmy_imyf7n.jpg",
+            url: "https://miro.medium.com/v2/resize:fit:2400/1*Atv7s1TO3VSEfNpq_2nc1A.jpeg"
+        },
+        {
+            id: "headingSeven",
+            target: "collapseSeven",
+            title: "Food Ordering System",
+            description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+            image: "https://res.cloudinary.com/dmfljlyu1/image/upload/v1687292391/potfolioappmy_imyf7n.jpg",
+            url: "https://miro.medium.com/v2/resize:fit:2400/1*Atv7s1TO3VSEfNpq_2nc1A.jpeg"
+        }
+    ])
+
     return (
         <div>
-            <section id="services">
+            <br/>
+            {/* <section id="services">
         <div className="container">
             <h1 className="text-center">My Projects</h1>
             <div className="row">
@@ -83,80 +144,32 @@ const MyProjects = (props: Props) => {
                 </div>
             </div>
         </div>
-    </section>
+    </section> */}
 
             <div className="container">
                 <div className="accordion" id="accordionExample">
-                    <div className="item">
-                        <div className="item-header" id="headingOne">
-                            <h2 className="mb-0">
-                                <button className="btn btn-link" type="button" data-toggle="collapse"
-                                    data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    Collapsible Item #1
-                                    <i className="fa fa-angle-down"></i>
-                                </button>
-                            </h2>
-                        </div>
-                        <div id="collapseOne" className="collapse" aria-labelledby="headingOne"
-                            data-parent="#accordionExample">
-                            <div className="t-p">
-                                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="item-header" id="headingTwo">
-                            <h2 className="mb-0">
-                                <button className="btn btn-link collapsed" type="button" data-toggle="collapse"
-                                    data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    Collapsible Item #2
-                                    <i className="fa fa-angle-down"></i>
-                                </button>
-                            </h2>
-                        </div>
-                        <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo"
-                            data-parent="#accordionExample">
-                            <div className="t-p">
-                                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="item-header" id="headingThree">
-                            <h2 className="mb-0">
-                                <button className="btn btn-link collapsed" type="button" data-toggle="collapse"
-                                    data-target="#collapseThree" aria-expanded="false"
-                                    aria-controls="collapseThree">
-                                    Collapsible Item #3
-                                    <i className="fa fa-angle-down"></i>
-                                </button>
-                            </h2>
-                        </div>
-                        <div id="collapseThree" className="collapse" aria-labelledby="headingThree"
-                            data-parent="#accordionExample">
-                            <div className="t-p">
-                                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="item-header" id="headingFour">
-                            <h2 className="mb-0">
-                                <button className="btn btn-link collapsed" type="button" data-toggle="collapse"
-                                    data-target="#collapseFour" aria-expanded="false"
-                                    aria-controls="collapseFour">
-                                    Collapsible Item #4
-                                    <i className="fa fa-angle-down"></i>
-                                </button>
-                            </h2>
-                        </div>
-                        <div id="collapseFour" className="collapse" aria-labelledby="headingFour"
-                            data-parent="#accordionExample">
-                            <div className="t-p">
-                                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-                            </div>
-                        </div>
-                    </div>
+                    {projects.map((project) => (
+                          <div className="item">
+                          <div className="item-header" id={project.id}>
+                              <h2 className="mb-0">
+                                  <button className="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                      data-target={`#${project.target}`} aria-expanded="true" aria-controls={project.target}>
+                                        {project.title}
+                                      <i className="fa fa-angle-down"></i>
+                                  </button>
+                              </h2>
+                          </div>
+                          <div id={project.target} className="collapse" aria-labelledby={project.id}
+                              data-parent="#accordionExample">
+                              <div className="t-p">
+                                  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                              </div>
+                                <div className="t-p">
+                                    <img src={project.image} alt="" className="img-fluid" />
+                                </div>
+                          </div>
+                      </div>
+                    ))}
                 </div>
             </div>
         </div>
