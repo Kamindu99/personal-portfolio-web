2650db172c85564f73a3c5380595728fd6fc36d6
Kamindu Gayantha <kamidugayantha123@gmail.com>
2024-02-20 02:29:55 +0530

    set blogs card

--- src/pages/MyBlogs.tsx
+++ src/pages/MyBlogs.tsx
@@ -6,6 +6,7 @@ function MyBlogs() {
         {
             id: 1,
             topTitle: '5 min read · Sep 16, 2023',
+            image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*rsdyu4-uIS2JpHDEi3p02w.png",
             title: "AngularJS for Beginners - Building Your First AngularJS Web App",
             description: "AngularJS is an open-source JavaScript framework developed by Google for building dynamic web applications.",
             link: "https://medium.com/@kamindugayantha/angularjs-for-beginners-building-your-first-angularjs-web-app-55115a5f4404"
@@ -13,6 +14,7 @@ function MyBlogs() {
         {
             id: 2,
             topTitle: '3 min read · Aug 22, 2023',
+            image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*rsdyu4-uIS2JpHDEi3p02w.png",
             title: "Building a CRUD API with Spring Boot: A Step-by-Step Guide",
             description: "Spring Boot has revolutionized Java application development by providing a powerful and streamlined framework for building robust and scalable applications. ",
             link: "https://medium.com/@kamindugayantha/angularjs-for-beginners-building-your-first-angularjs-web-app-55115a5f4404"
@@ -20,6 +22,7 @@ function MyBlogs() {
         {
             id: 1,
             topTitle: '5 min read · Sep 16, 2023',
+            image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*rsdyu4-uIS2JpHDEi3p02w.png",
             title: "AngularJS for Beginners - Building Your First AngularJS Web App",
             description: "AngularJS is an open-source JavaScript framework developed by Google for building dynamic web applications.",
             link: "https://medium.com/@kamindugayantha/angularjs-for-beginners-building-your-first-angularjs-web-app-55115a5f4404"
@@ -27,12 +30,15 @@ function MyBlogs() {
         {
             id: 2,
             topTitle: '3 min read · Aug 22, 2023',
+            image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*rsdyu4-uIS2JpHDEi3p02w.png",
             title: "Building a CRUD API with Spring Boot: A Step-by-Step Guide",
             description: "Spring Boot has revolutionized Java application development by providing a powerful and streamlined framework for building robust and scalable applications. ",
             link: "https://medium.com/@kamindugayantha/angularjs-for-beginners-building-your-first-angularjs-web-app-55115a5f4404"
-        }, {
+        },
+        {
             id: 1,
             topTitle: '5 min read · Sep 16, 2023',
+            image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*rsdyu4-uIS2JpHDEi3p02w.png",
             title: "AngularJS for Beginners - Building Your First AngularJS Web App",
             description: "AngularJS is an open-source JavaScript framework developed by Google for building dynamic web applications.",
             link: "https://medium.com/@kamindugayantha/angularjs-for-beginners-building-your-first-angularjs-web-app-55115a5f4404"
@@ -40,36 +46,24 @@ function MyBlogs() {
         {
             id: 2,
             topTitle: '3 min read · Aug 22, 2023',
+            image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*rsdyu4-uIS2JpHDEi3p02w.png",
             title: "Building a CRUD API with Spring Boot: A Step-by-Step Guide",
             description: "Spring Boot has revolutionized Java application development by providing a powerful and streamlined framework for building robust and scalable applications. ",
             link: "https://medium.com/@kamindugayantha/angularjs-for-beginners-building-your-first-angularjs-web-app-55115a5f4404"
-        }, {
-            id: 1,
-            topTitle: '5 min read · Sep 16, 2023',
-            title: "AngularJS for Beginners - Building Your First AngularJS Web App",
-            description: "AngularJS is an open-source JavaScript framework developed by Google for building dynamic web applications.",
-            link: "https://medium.com/@kamindugayantha/angularjs-for-beginners-building-your-first-angularjs-web-app-55115a5f4404"
-        },
-        {
-            id: 2,
-            topTitle: '3 min read · Aug 22, 2023',
-            title: "Building a CRUD API with Spring Boot: A Step-by-Step Guide",
-            description: "Spring Boot has revolutionized Java application development by providing a powerful and streamlined framework for building robust and scalable applications. ",
-            link: "https://medium.com/@kamindugayantha/angularjs-for-beginners-building-your-first-angularjs-web-app-55115a5f4404"
-        },
+        }
     ]
 
     return (
-        <div className='myblogs '>
+        <div className='myblogs'>
             <div className="container bootstrap snippets bootdeys">
                 <div className="row">
                     {myblogs.map((blog) => (
-                        <div className="col-md-4 col-sm-6 content-card">
+                        <div className="col-md-3 col-sm-6 content-card" >
                             <div className="card-big-shadow">
-                                <div className="card card-just-text" data-background="color" data-color="purple" data-radius="none">
+                                <div className="card card-just-text" data-background="color" data-color="purple" data-radius="none" style={{ cursor: 'pointer' }} onClick={() => { window.open(blog?.link, '_blank'); }}>
                                     <div className="content">
+                                        <h4 className="title">{blog?.title}</h4>
                                         <h6 className="category">{blog?.topTitle}</h6>
-                                        <h4 className="title"><a href={blog?.link}>{blog?.title}</a></h4>
                                         <p className="description">{blog?.description} </p>
                                     </div>
 
@@ -81,7 +75,7 @@ function MyBlogs() {
                 </div>
             </div>
 
-        </div>
+        </div >
     )
 }
 
