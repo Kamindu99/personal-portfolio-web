6c7b2f4e1b4705e9c47d6d51a3aa31dec6860ae3
Kamindu Gayantha <kamidugayantha123@gmail.com>
2024-02-20 02:46:18 +0530

    update blogs

--- src/pages/MyBlogs.tsx
+++ src/pages/MyBlogs.tsx
@@ -34,6 +34,38 @@ function MyBlogs() {
             title: "Become a Successful Full Stack Developer through the OOP Principles and Practices",
             description: "Application Frameworks An application framework is a software library that provides the basic structure to assist in the development",
             link: "https://medium.com/@kamindugayantha/become-a-successful-full-stack-developer-through-the-oop-principles-and-practices-39057c13c0b4"
+        },
+        {
+            id: 5,
+            topTitle: '5 min read · May 15, 2022',
+            image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*rsdyu4-uIS2JpHDEi3p02w.png",
+            title: "JavaScript concepts you need to know as a JavaScript programmer!",
+            description: "JavaScript (Js) is a programming language that integrates HTML and CSS with one of the basic technologies in the World Wide Web. More",
+            link: "https://medium.com/@kamindugayantha/javascript-concepts-you-need-to-know-as-a-javascript-programmer-1b2dc74cd1f3"
+        },
+        {
+            id: 6,
+            topTitle: '4 min read · May 15, 2022',
+            image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*rsdyu4-uIS2JpHDEi3p02w.png",
+            title: "ReactJS for Frontend Development",
+            description: "What is ReactJS React is a JavaScript based UI development library designed to facilitate the development of visual interfaces. It is run by Facebook and the open source developer community. Although React is",
+            link: "https://medium.com/@kamindugayantha/reactjs-for-the-frontend-development-ea7cfa59110f"
+        },
+        {
+            id: 7,
+            topTitle: '4 min read · May 15, 2022',
+            image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*rsdyu4-uIS2JpHDEi3p02w.png",
+            title: "Node JS for Beginners",
+            description: "What is NODEJS Node.js is an open source and cross-platform runtime environment for executing JavaScript code outside of a browser. Many people are confused and understand that it is a framework or",
+            link: "https://medium.com/@kamindugayantha/node-js-for-beginners-39fc34e6468"
+        },
+        {
+            id: 8,
+            topTitle: '4 min read · May 15, 2022',
+            image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*rsdyu4-uIS2JpHDEi3p02w.png",
+            title: "What is KoaJS Framework",
+            description: "When it comes to web development, there are times when you need to choose a framework to enable and activate your backend API. …",
+            link: "https://medium.com/@kamindugayantha/what-is-koajs-framework-fb28876c7509"
         }
     ]
 
