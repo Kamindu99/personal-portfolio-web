8e30e5ee4e19d5fdcaa93e10fed2d94d3b9e6b1a
Kamindu Gayantha <kamidugayantha123@gmail.com>
2024-02-24 00:31:51 +0530

    add new blogs

--- src/pages/MyBlogs.tsx
+++ src/pages/MyBlogs.tsx
@@ -3,7 +3,26 @@ import React from 'react'
 function MyBlogs() {
 
     const myblogs = [
-
+        {
+            id: 1,
+            readingTime: '5 min read',
+            addedDate: "Sep 16, 2023",
+            category: 'AngularJS',
+            image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*NUZCLQfkiyyZtduHTC_6fA.jpeg",
+            title: "AngularJS for Beginners - Building Your First AngularJS Web App",
+            description: "AngularJS is an open-source JavaScript framework developed by Google for building dynamic web applications.",
+            link: "https://medium.com/@kamindugayantha/angularjs-for-beginners-building-your-first-angularjs-web-app-55115a5f4404"
+        },
+        {
+            id: 2,
+            readingTime: '3 min read',
+            addedDate: "Aug 22, 2023",
+            category: 'Spring Boot',
+            image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*rsdyu4-uIS2JpHDEi3p02w.png",
+            title: "Building a CRUD API with Spring Boot: A Step-by-Step Guide",
+            description: "Spring Boot has revolutionized Java application development by providing a powerful and streamlined framework for building robust and scalable applications. ",
+            link: "https://medium.com/@kamindugayantha/building-a-crud-application-with-spring-boot-a-step-by-step-guide-9926a2e1349"
+        },
         {
             id: 3,
             readingTime: '4 min read',
