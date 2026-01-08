dbe021419a6e318568b46f1f0c2f53af61d1d088
kamindu99 <kamidugayantha123@gmail.com>
2026-01-08 10:28:29 +0530

    update images

--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -2,11 +2,9 @@ const breakpoints = [3840, 2400, 1080, 640, 384, 256, 128, 96, 64, 48];
 
 const unsplashPhotos = [
     { id: "https://miro.medium.com/v2/resize:fit:2400/1*Atv7s1TO3VSEfNpq_2nc1A.jpeg", width: 900, height: 800 },
-    { id: "https://media.licdn.com/dms/image/C5603AQGnBMwskpBqvw/profile-displayphoto-shrink_800_800/0/1656409186148?e=2147483647&v=beta&t=1JY6ZWlcPp7IySxX-_I6ECkodmTMZFKR4WA1K5nl75A", width: 1080, height: 1000 },
     { id: "https://pbs.twimg.com/media/E0uDvutUYAEIRsR.jpg:large", width: 1180, height: 1620 },
     { id: "https://pbs.twimg.com/media/FZSpEqFUIAAZIX9?format=jpg&name=large", width: 800, height: 707 },
     { id: "https://pbs.twimg.com/media/E0tEXSVVcAAThjq.jpg:large", width: 900, height: 608 },
-    { id: "https://media.licdn.com/dms/image/C4E16AQF7KGZv4tFVFQ/profile-displaybackgroundimage-shrink_200_800/0/1623943939422?e=2147483647&v=beta&t=ZCtdM13ByYalJjk7nROD1ccnnu-rhEUf6_bDN-dHsQk", width: 1080, height: 260 },
     { id: "https://res.cloudinary.com/dmfljlyu1/image/upload/v1687378619/MyImages/Myimages_6_e7oras.jpg", width: 800, height: 608 },
     { id: "https://res.cloudinary.com/dmfljlyu1/image/upload/v1687378619/MyImages/Myimages_2_lc8pbn.jpg", width: 900, height: 1408 },
     { id: "https://res.cloudinary.com/dmfljlyu1/image/upload/v1687378619/MyImages/Myimages_5_od2frq.jpg", width: 400, height: 608 },
